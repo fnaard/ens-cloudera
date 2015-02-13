953fdc9ff0da2588a71f4eab63b5393644ed91a4
Gabriel M Schuyler <gabe@puppetlabs.com>
2015-02-13 09:44:02 -0800

    Add comments and tidy

--- modules/profile/manifests/base.pp
+++ modules/profile/manifests/base.pp
@@ -1,4 +1,11 @@
-
+# This class manages a few things that would normally be already managed
+# by your existing profile::base module.  It is provided here as a convenience
+# when testing the profile::cdh:: classes.
+#
+# For instance, most sites already manage the presence, owner and permissions
+# of /root/.ssh.  However, on a test node, the profile::cdh:: classes may be
+# being applied by themselves.  Including this class simulates a machine
+# where a pre-existing profile::base would have been normally installed.
 
 class profile::base {
 
@@ -8,10 +15,8 @@ class profile::base {
     group  => 'root',
     mode   => '0500',
   }
-
-  package { [ 'ntpdate', 'git' ]:
+  package { [ 'ntpdate' ]:
     ensure => present,
   }
-
 
 }
